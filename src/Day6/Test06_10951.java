02ac31b729b853131a6561d906919897a2f0c666
임동혁 <as0387@naver.com>
2021-12-24 01:43:55 +0900

    백준 6일차 문제 1110번 못풀음

---
+++ src/Day6/Test06_10951.java
@@ -0,0 +1,26 @@
+package Day6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test06_10951 {
+    //while문 1번 A+B
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while((str=br.readLine())!= null) {
+            st = new StringTokenizer(str, " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A + B).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/Day6/Test06_1110.java
@@ -0,0 +1,12 @@
+package Day6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test06_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
