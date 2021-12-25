032ddf75fb8f58d71d498a86cc9265abbd70d764
임동혁 <as0387@naver.com>
2021-12-25 22:26:09 +0900

    백준 7일차 문제 1110번 품!

--- src/Day6/Test06_1110.java
+++ src/Day6/Test06_1110.java
@@ -8,5 +8,17 @@ public class Test06_1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int inputNum = Integer.parseInt(br.readLine());
+        br.close();
+        int resultNum = inputNum;
+        int count = 0;
+        while(true){
+            int tenDigit = (resultNum % 10) * 10;
+            int onetDigit = ((resultNum / 10) + (resultNum % 10)) % 10;
+            resultNum = tenDigit + onetDigit;
+            count++;
+            if (inputNum == resultNum) break;
+        }
+        System.out.println(count);
     }
 }
