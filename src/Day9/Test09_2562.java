5fbcd1cd1db8bc840b8033c07e6949a132ca3966
임동혁 <as0387@naver.com>
2021-12-27 23:42:38 +0900

    백준 9일차

---
+++ src/Day9/Test09_2562.java
@@ -0,0 +1,32 @@
+package Day9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test09_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        List<Integer> list = new ArrayList<>();
+//
+//        list.add(Integer.parseInt(br.readLine()));
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int max = arr[0];
+        int num = 0;
+        for (int i:
+             arr) {
+            num++;
+            if (max < i) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(num);
+    }
+}
