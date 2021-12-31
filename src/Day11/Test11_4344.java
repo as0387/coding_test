d25010c5ea4f853d9a3f9c68b995194db6f1b9da
임동혁 <as0387@naver.com>
2021-12-31 23:36:27 +0900

    백준 13일차

--- src/Day11/Test11_4344.java
+++ src/Day11/Test11_4344.java
@@ -1,7 +1,17 @@
 package Day11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Test11_4344 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer sr;
+        for (int i = 0; i < N; i++) {
+            int scoreNo = Integer.parseInt("asd");
+        }
     }
 }
