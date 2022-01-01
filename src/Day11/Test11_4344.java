0bea817f74df56f09a2ab23447770759492cc0e6
임동혁 <as0387@naver.com>
2022-01-01 21:35:45 +0900

    백준 14일차

--- src/Day11/Test11_4344.java
+++ src/Day11/Test11_4344.java
@@ -10,8 +10,25 @@ public class Test11_4344 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer sr;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int scoreNo = Integer.parseInt("asd");
+            sr = new StringTokenizer(br.readLine(), " ");
+            int[] scoreArr = new int[Integer.parseInt(sr.nextToken())];
+            int sum = 0;
+
+            for (int j = 0; j < scoreArr.length; j++) {
+                scoreArr[j] = Integer.parseInt(sr.nextToken());
+                sum += scoreArr[j];
+            }
+
+            int avg = sum / scoreArr.length;
+            int overAvgCount = 0;
+            for (int j : scoreArr) {
+                if(j > avg) overAvgCount++;
+            }
+            String result = String.format("%.3f", ((double)overAvgCount/(double)scoreArr.length) * 100);
+            sb.append(result).append("%").append("\n");
         }
+        System.out.println(sb);
     }
 }
