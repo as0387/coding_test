2d9e044cf5c42841f61bc16442a529729bf91cb0
임동혁 <as0387@naver.com>
2022-01-13 13:24:42 +0900

    백준 20일차

--- src/Day18/Test18_4673.java
+++ src/Day18/Test18_4673.java
@@ -4,26 +4,30 @@ import java.util.ArrayList;
 
 public class Test18_4673 {
 
-    public static int d(int n){
-        int sum = n;
 
-        while(n != 0){//1자리씩 없애는 법 기억해!
-            sum += n % 10;//첫번째 자리
-            n /= 10;
-        }
-        return sum;
-    }
     public static void main(String[] args) {
         boolean[] flag = new boolean[10001];
-        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i < 10001; i++) {
             int n = d(i);
+
             if(n < 10001) flag[n] = true;
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i < 10001; i++) {
-            if(!flag[i]) sb.append(i).append("\n");//false가 기본값이니 true아닌거가 없는 숫자임
+            if(flag[i]) sb.append(i).append("\n");
         }
         System.out.println(sb);
+    }
+
+    private static int d(int n) {
+        //일의 자리부터 하나씩 더함
+        int sum = n;
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;//1의자리씩 제거
+        }
+        return sum;
     }
 }
